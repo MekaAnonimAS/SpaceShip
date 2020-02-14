afba046a1fda459fe7925a80c8073c1186e55469
malidesidero <mehmet.ali.mutlul4r@gmail.com>
2020-02-14 23:17:57 +0300

    NEWSCRİPT
    EHEHH

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   
+  
+    int i = 0;
+    // Update is called once per frame
+    void Update()
+    {
+       i++;
+    }
+}
